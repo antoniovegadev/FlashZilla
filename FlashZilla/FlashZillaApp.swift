8a2fa9e260c896d6ef06ded2bddc274779e86515
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-15 22:13:02 -0500

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Antonio Vega on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
